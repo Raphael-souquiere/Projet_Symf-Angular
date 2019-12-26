b5259e5b4d1b4d94ab161780542c4ba9b1e8261a
Raphael-souquiere <51905489+Raphael-souquiere@users.noreply.github.com>
2019-12-26 12:25:29 +0100

    oui

---
+++ Symfony/src/BackBundle/Entity/Etat.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BackBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Etat
+ *
+ * @ORM\Table(name="etat")
+ * @ORM\Entity
+ */
+class Etat
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cause_arret", type="string", length=50, nullable=false)
+     */
+    private $causeArret;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+
+
+    /**
+     * Set causeArret
+     *
+     * @param string $causeArret
+     *
+     * @return Etat
+     */
+    public function setCauseArret($causeArret)
+    {
+        $this->causeArret = $causeArret;
+
+        return $this;
+    }
+
+    /**
+     * Get causeArret
+     *
+     * @return string
+     */
+    public function getCauseArret()
+    {
+        return $this->causeArret;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
